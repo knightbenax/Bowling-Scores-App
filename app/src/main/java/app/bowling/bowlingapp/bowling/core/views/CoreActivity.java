45ad6a1cef8826672866f5e33c76ee6ca9ba34eb
Bezaleel Ashefor <knightbenax@gmail.com>
2020-05-09 09:22:57 +0100

    Getting ready to add IAP

--- app/src/main/java/app/bowling/bowlingapp/bowling/core/views/CoreActivity.java
+++ app/src/main/java/app/bowling/bowlingapp/bowling/core/views/CoreActivity.java
@@ -55,6 +55,8 @@ public class CoreActivity extends AppCompatActivity {
     }
 
 
+
+
     public static void hideKeyboard(Activity activity) {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         //Find the currently focused view, so we can grab the correct window token from it.
